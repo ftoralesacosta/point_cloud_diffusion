9a996e8ebe3eb01c45067f8835eccb4cc21f7971
ftoralesacosta <ftoralesacosta@lbl.gov>
2024-04-03 09:19:20 -0700

    updated embedding class

--- models/point_cloud_diffusion.py
+++ models/point_cloud_diffusion.py
@@ -1,104 +1,169 @@
+import torch
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers, Input
-import time
-import utils
-from deepsets import DeepSetsAtt, Resnet
-from tensorflow.keras.activations import swish, relu
+import yaml
+import torch.nn as nn
+from deepsets import DeepSetsAtt
+import torch.nn.functional as F
 
-tf.random.set_seed(1235)
+activation = nn.LeakyReLU(0.01)
 
-class GSGM(keras.Model):
+class PCD(nn.Module):  # Point Cloud Diffusion
     """Score based generative model"""
 
-    def __init__(self,name='SGM',npart=30,config=None,factor=1):
+    def __init__(self, name='SGM', npart=30, config_file=None, factor=1):
+        super(PCD, self).__init__()
 
-        super(GSGM, self).__init__()
+        if config_file is None:
+            raise ValueError("\nNeed to specify config file!!\n")
 
+        config = yaml.safe_load(open(config_file))
         self.config = config
-        if config is None:
-            raise ValueError("\nConfig File Not Specified\n")
 
-
-        self.activation = layers.LeakyReLU(alpha=0.01)
-        self.factor=factor
-        # Input Data parameters
+        self.factor = factor
         self.num_feat = self.config['NUM_FEAT']
         self.num_cluster = self.config['NUM_CLUS']
         self.num_cond = self.config['NUM_COND']
         self.num_embed = self.config['EMBED']
-        self.max_part = npart
         self.num_steps = self.config['MAX_STEPS']//self.factor
-        self.ema=0.999
+        self.ema = 0.999
 
-        # Diffusion parameters
-        self.timesteps = tf.range(start=0,limit=self.num_steps + 1, dtype=tf.float32) / self.num_steps + 8e-3 
-        alphas = self.timesteps / (1 + 8e-3) * np.pi / 2.0
-        alphas = tf.math.cos(alphas)**2
-        alphas = alphas / alphas[0]
-        betas = 1 - alphas[1:] / alphas[:-1]
-        self.betas = tf.clip_by_value(betas, clip_value_min =0, clip_value_max=0.999)
-        alphas = 1 - self.betas
-        self.alphas_cumprod = tf.math.cumprod(alphas, 0)
-        alphas_cumprod_prev = tf.concat((tf.ones(1, dtype=tf.float32), self.alphas_cumprod[:-1]), 0)
-        self.posterior_variance = self.betas * (1 - alphas_cumprod_prev) / (1. - self.alphas_cumprod)
-        self.posterior_mean_coef1 = (self.betas * tf.sqrt(alphas_cumprod_prev) / (1. - self.alphas_cumprod))
-        self.posterior_mean_coef2 = (1 - alphas_cumprod_prev) * tf.sqrt(alphas) / (1. - self.alphas_cumprod)
-        self.projection = self.GaussianFourierProjection(scale = 16)
+        # Diffusion TimeSteps
+        self.timesteps = torch.arange(0, self.num_steps + 1, 
+                dtype=torch.float32) / self.num_steps + 8e-3
 
-        #self.verbose = 1 if hvd.rank() == 0 else 0 #show progress only for first rank
-        self.loss_tracker = keras.metrics.Mean(name="loss")
+        # Diffusion Parameters, for the denoising and score learning
+        self.Set_alpha_beta_posterior()
 
-        #Transformation applied to conditional inputs
-        inputs_time = Input((1))
-        inputs_cond = Input((self.num_cond)) # shape=(None, 2) 2 
-        inputs_cluster = Input((self.num_cluster)) # shape=(None, 2) 2 
+        self.projection = self.GaussianFourierProjection(scale=16)
 
-        print("*"*10,"GSGM.py: 59","*"*10)
-        print('inputs_cluster', inputs_cluster)
-        print('inputs_cond',inputs_cond)
+        self.loss_tracker = nn.MSELoss()
+        # keras.metrics.Mean(name="loss")
 
+        # DEFINE THE INPUT LAYERS
+        inputs_time = torch.tensor(1)
+        inputs_cond = torch.tensor((self.num_cond))
+        inputs_cluster = torch.tensor(self.num_cluster)
+        inputs_mask = torch.tensor((1, 1)) # second eleme = feature size
+        # ^TF version in (None,1). Torch can handle dynamic
+        # input size, but the feature size (second dim)
+        # must match. The mask has feature dim = 1
 
+        # Activation function
+        self.activation = nn.LeakyReLU(0.01)
 
-        # Defining the Model
-        inputs_mask = Input((None,1)) #mask to identify zero-padded objects       
+        # DEFNE GRAPH MODEL
+        self.graph_embedding1 = Embedding(self.num_embed)
+        self.graph_linear1 = nn.Linear(self.num_embed, self.num_embed)
+        self.graph_activation1 = self.activation
 
-        graph_conditional = self.Embedding(inputs_time,self.projection) # shape=(None, 64) print('graph_conditional',graph_conditional)
-        cluster_conditional = self.Embedding(inputs_time,self.projection) # shape=(None, 64) print('cluster_conditional',cluster_conditional)      
-        
-        print('graph_conditional 0',graph_conditional) # shape=(None, 64)
-        print('cluster_conditional 0',cluster_conditional) # shape=(None, 64)
-        
-        graph_conditional = layers.Dense(self.num_embed,activation=None)(tf.concat(
-            [graph_conditional,inputs_cluster,inputs_cond],-1))
-        graph_conditional=self.activation(graph_conditional)
-        
-        cluster_conditional = layers.Dense(self.num_embed,activation=None)(tf.concat(
-            [cluster_conditional,inputs_cond],-1))
-        cluster_conditional=self.activation(cluster_conditional)
+        # DEFINE CLUSTER MODEL
+        self.cluster_embedding1 = Embedding(self.num_embed)
+        self.cluster_linear1 = nn.Linear(self.num_embed, self.num_embed)
+        self.cluster_activation1 = self.activation
 
-        # These outputs for graph_conditional and cluster_conditional are (None,64) as they pass through embedding sizedense layer.
-        print('graph_conditional 1',graph_conditional) # shape=(None, 64)
-        print('cluster_conditional 1',cluster_conditional) # shape=(None, None, 64)
-        # These conditionals will now get attached to Inputs
-        # x1 + 
+        # the first dim will be n_cluster or n_part
 
-        # This block is only for cells, where the input x is of dim (None,None,4) (x,y,z,E)
-        self.shape = (-1,1,1)
-        inputs,outputs = DeepSetsAtt(
-            num_feat=self.num_feat,
+    def forward(self, inputs_time, inputs_cond, inputs_cluster, inputs_mask):
+
+        # Graph Forward Pass
+        graph_conditional = self.Embedding(inputs_time, self.projection)
+        graph_inputs = torch.cat([graph_conditional,inputs_cluster,inputs_cond],-1)
+        graph_conditional = nn.Linear(graph_inputs, self.num_embed)
+        graph_conditional = self.activation(graph_conditional)
+
+        # Cluster Forward Pass
+        cluster_conditional = self.Embedding(inputs_time, self.projection)
+        cluster_inputs = torch.cat([cluster_conditional, inputs_cond], -1)
+        cluster_conditional = nn.Linear(cluster_inputs, self.num_embed)
+        cluster_conditional = self.activation(cluster_conditional)
+
+        inputs, outputs = DeepSetsAtt(
+            num_feat=self.num_embed,
             time_embedding=graph_conditional,
             num_heads=1,
-            num_transformer = 8, #nominal 8
-            projection_dim = 64,
-            mask = inputs_mask,
+            num_transformer=8,
+            projection_dim=64,
+            mask=inputs_mask,
         )
 
-        print(f"inputs = {inputs}") # inputs = KerasTensor(type_spec=TensorSpec(shape=(None, None, 4), dtype=tf.float32, name='input_5'), name='input_5', description="created by layer 'input_5'")
-        print(f"outputs1 = {outputs}") # outputs1 = KerasTensor(type_spec=TensorSpec(shape=(None, None, 4), dtype=tf.float32, name=None), name='time_distributed_5/Reshape_1:0', description="created by layer 'time_distributed_5'")
-        print('num_cluster', self.num_cluster) # num_cluster 2
-        
-        # (None, None, 4){x,y,z,E} , (None, 64){time embedding_attaches to each particle differently} , cluster = (None, 2){Number, E_cl} , inputs_cond = (None, 2) {Pgen, Theta}, (None,1){For masking E value cell < 0}
-        self.model_part = keras.Model(inputs=[inputs,inputs_time,inputs_cluster,inputs_cond,inputs_mask],outputs=outputs)
-        # outputs = (None, None, 4)
+        # self.model_part = keras.Model(inputs=[inputs,inputs_time,inputs_cluster,inputs_cond,inputs_mask],outputs=outputs)
+
+        return outputs
+
+    def Set_alpha_beta_posterior(self):
+        # The math behind diffusion
+        # Cumulative alphas, s.t. you don't
+        # have to calculate noise at each step
+
+        alphas = self.timesteps / (1 + 8e-3) * np.pi / 2.0
+        alphas = torch.cos(alphas)**2
+        alphas = alphas / alphas[0]
+        betas = 1 - alphas[1:] / alphas[:-1]
+        betas = torch.clamp(betas, min=0, max=0.999)
+        alphas = 1 - betas
+
+        alphas_cumprod = torch.cumprod(alphas, 0)
+        alphas_cumprod_prev = torch.cat((torch.ones(1, dtype=torch.float32),
+                                         alphas_cumprod[:-1]), 0)
+
+        # set
+        self.betas = betas
+        self.alphas_cumprod = alphas_cumprod
+
+        self.posterior_variance = self.betas * \
+            (1 - alphas_cumprod_prev) / (1. - self.alphas_cumprod)
+
+        self.posterior_mean_coef1 = (self.betas * \
+            torch.sqrt(alphas_cumprod_prev) / (1. - self.alphas_cumprod))
+
+        self.posterior_mean_coef2 = (1 - alphas_cumprod_prev) * \
+            torch.sqrt(alphas) / (1. - self.alphas_cumprod)
+
+
+    def GaussianFourierProjection(self, scale=30):
+        half_dim = self.num_embed // 4
+        emb = torch.log(torch.tensor(10000.0)) / (half_dim - 1)
+        freq = torch.exp(-emb * torch.arange(0, half_dim, dtype=torch.float32))
+        return freq
+
+
+class Embedding(nn.Module):
+    def __init__(self, num_embed):
+        super(Embedding, self).__init__()
+
+        self.num_embed = num_embed
+        self.dense1 = nn.Linear(2 * self.num_embed, 2 * self.num_embed)
+        self.dense2 = nn.Linear(2 * self.num_embed, self.num_embed)
+
+
+    def forward(self, inputs, projection):
+
+        angle = inputs * projection
+        embedding = torch.cat((torch.sin(angle), torch.cos(angle)), dim=-1)
+
+        embedding = self.dense1(embedding)
+        embedding = F.leaky_relu(embedding, 0.01)
+        embedding = self.dense2(embedding)
+        return embedding
+
+
+
+
+
+
+
+
+        # angle shape = (None, 16)
+        # Num embed =  64
+        # SIZE after concat sin, cos FUNCTION =  (None, 32)
+        # SIZE of Dense Layer =  (None, 128)
+        # num_embed 64
+        # SIZE dense(num_embed)*embedding=  (None, 64)
+
+    # def count_parameters(model):
+    #     total_params = sum(p.numel() for p in model.parameters())
+    #     return total_params
+
+    # num_params = count_parameters(self.model_part)
+    # print("Number of parameters: {:,}".format(num_params))
+    # Number of parameters: 314,372
