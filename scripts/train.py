ebe715571c18a0612b231fa8354d3efca2cfa380
ftoralesacosta <ftoralesacosta@lbl.gov>
2024-04-18 12:30:55 -0700

    dataloader for cells+clusters. Debug training

--- scripts/train.py
+++ scripts/train.py
@@ -1,59 +1,139 @@
-# JUST A SKELETON FOR NOW
+import argparse
 import torch
+import torch.distributed as dist
+import torch.nn as nn
 import torch.optim as optim
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
+import utils
+import data_loading
+import time
 import sys
-import yaml
+import data_loading
+sys.path.insert(1, '../models/')
 from point_cloud_diffusion import PCD
-sys.path.insert(1, '/pscratch/sd/f/fernando/point_cloud_diffusion/models/')
-# import diffusion_PyTorch
+# from GSGM_distill import GSGM_distill
 
-# Assuming the PCD class is already defined
-config_file = "../configs/default_config.yaml"
-model = PCD(config_file=config_file)  # Initialize your model
-optimizer = optim.Adam(model.parameters(), lr=0.001)  # Use the Adam optimizer
-criterion = torch.nn.MSELoss()  # Example loss function, change if necessary
+torch.manual_seed(1233)
 
-# Placeholder for your training data and labels
-# You will need to replace these with your actual data loading mechanism
-train_data = torch.rand(100, 64)  # Example input data
-train_labels = torch.rand(100, 64)  # Example labels/targets
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='../configs/config_cluster.json',
+                        help='Config file with training parameters')
 
-# FIXME: Update to grab dims from config
-inputs_time = torch.rand(1)  # FIXME: probably need (batch,1)
-inputs_cond = torch.rand(1)
-inputs_cluster = torch.rand(2)
-inputs_mask = torch.rand(1)
+    parser.add_argument('--data_path', default='../data/',
+                        help='Path containing the training files')
 
-# Number of epochs
-num_epochs = 10
+    parser.add_argument('--distill', action='store_true',
+                        default=False, help='Use the distillation model')
 
-# Training loop
-for epoch in range(num_epochs):
-    model.train()  # Set the model to training mode
+    parser.add_argument('--big', action='store_true', default=False,
+                        help='Use bigger dataset (1000 particles) \
+                        as opposed to 200 particles')
 
-    optimizer.zero_grad()  # Clear the gradients
+    parser.add_argument('--factor', type=int, default=1,
+                        help='Step reduction for distillation model')
 
-    # Forward pass: Compute the model output for the input data
+    parser.add_argument('--local_rank', type=int, default=0,
+                        help='Local rank for DDP')
 
-    # exit("Not Ready model training yet!!!")
-    outputs = model(train_data,
-                    inputs_time,
-                    inputs_cluster,
-                    inputs_cond,
-                    inputs_mask)
+    flags = parser.parse_args()
 
-    # Compute the loss
-    loss = criterion(outputs, train_labels)
-    # This will be a custom loss function from PCD class
+    # dist.init_process_group(backend='nccl')
+    # torch.cuda.set_device(flags.local_rank)
 
-    # Backward pass: Compute gradient of the 
-    # loss with respect to model parameters
-    loss.backward()
+    config = utils.LoadJson(flags.config)
 
-    # Perform a single optimization step (parameter update)
-    optimizer.step()
+    assert flags.factor % 2 == 0 or flags.factor == 1,\
+    "Distillation reduction steps needs to be even"
 
-    print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+    if flags.big:
+        labels = utils.labels1000
+        npart = 1000
+    else:
+        labels = utils.labels200
+        npart = 200
 
-# After training, you can save the model if needed
-torch.save(model.state_dict(), 'gsgm_model.pth')
+    data_size, training_data, test_data = data_loading.MainDataLoader(flags.data_path,
+                                                           labels,
+                                                           npart,
+                                                           # dist.get_rank(),
+                                                           # dist.get_world_size(),
+                                                           config['NUM_CLUS'],
+                                                           config['NUM_COND'],
+                                                           config['BATCH'])
+
+    model = PCD(config=config, npart=npart).cuda()
+
+    model_name = config['MODEL_NAME']
+    if flags.big:
+        model_name += '_big'
+    checkpoint_folder = '../checkpoints_{}/checkpoint'.format(model_name)
+
+    # if flags.distill:
+    #     if flags.factor > 2:
+    #         checkpoint_folder = \
+    #         '../checkpoints_{}_{}/checkpoint'.format(model_name,flags.factor // 2)
+
+    #         model = GSGM_distill(model.ema_jet, model.ema_part,
+    #                              factor=flags.factor // 2, config=config)
+
+    #         model.load_state_dict(torch.load(checkpoint_folder))
+
+    #         model = GSGM_distill(model.ema_jet, model.ema_part,
+    #                              factor=flags.factor, config=config, npart=npart)
+    #     else:
+    #         model.load_state_dict(torch.load(checkpoint_folder))
+    #         model = GSGM_distill(model.ema_jet, model.ema_part,
+    #                              factor=flags.factor, config=config, npart=npart)
+
+    #     if dist.get_rank() == 0:
+    #         print(f"Loading Teacher from: {checkpoint_folder}")
+
+    #     checkpoint_folder = \
+    #     f'../checkpoints_{model_name}_d{flags.factor}/checkpoint'
+
+    model = DDP(model, device_ids=[flags.local_rank])
+
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optim.Adamax(model.parameters(), lr=config['LR']),
+        T_max=config['MAXEPOCH'] * int(data_size * 0.8 / config['BATCH'])
+    )
+
+    loss_fn = nn.MSELoss()
+    # FIXME: Do the below instead. You'll have to finish the GSGM Class tomorrow!!!
+    # loss = model.custom_loss(outputs, targets)
+
+    start_time = time.time()
+
+    # Optimizer
+    optimizer = optim.Adamax(model.parameters(), lr=config['LR'])
+
+    # Assuming training_data is a DataLoader
+    sampler = DistributedSampler(training_data.dataset,
+                                 num_replicas=dist.get_world_size(),
+                                 rank=dist.get_rank())
+
+    training_data.sampler = sampler
+
+    # for epoch in range(num_epochs):
+    #     for batch in train_loader:
+    #         particles, jets, conditions, mask = batch
+    #         print(particles, jets, conditions, mask)
+
+    # FIXME: adapt from train.py
+    for epoch in range(config['MAXEPOCH']):
+        model.train()
+        sampler.set_epoch(epoch)
+        for data, target in training_data:
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_fn(output, target)
+            loss.backward()
+            optimizer.step()
+        lr_scheduler.step()
+
+        if dist.get_rank() == 0 and epoch % 1 == 0:
+            torch.save(model.state_dict(), checkpoint_folder)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
